02c763e7e23f385d62fa2c55a6927422b6516fde
umebosh <gsd.strike.f.007.031@gmail.com>
2015-12-13 04:07:23 +0900

    dirt convert into diamond_block

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,9 +1,14 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
@@ -11,10 +16,12 @@ public class ExampleMod
     public static final String MODID = "examplemod";
     public static final String VERSION = "1.0";
     
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        GameRegistry.addRecipe(new ItemStack(Blocks.diamond_block, 64),
+                "X", "X", "X",
+                'X', Blocks.dirt);
     }
 }
